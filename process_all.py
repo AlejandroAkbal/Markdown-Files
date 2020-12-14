3ced976e19cd79e5760054cf1fffc30e8d1e4c11
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2020-12-14 10:34:46 +0100

    feat: create python script for automatic processing

---
+++ process_all.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+from MarkdownPP import MarkdownPP
+
+modules = ['include']
+
+current_dir_utility = Path('.')
+
+list_of_files_that_match = current_dir_utility.rglob('*.mdpp')
+
+for file in list_of_files_that_match:
+    input_file = Path.absolute(file)
+    input_file_data = open(input_file, 'r')
+
+    output_file = f'{input_file.stem}.processed.md'
+    output_file_data = open(output_file, 'w')
+
+    print(f'File: {file}')
+    # print(f'Absolute path: {input_file}')
+
+    MarkdownPP(input=input_file_data, output=output_file_data, modules=modules)
+
+    input_file_data.close()
+    output_file_data.close()
